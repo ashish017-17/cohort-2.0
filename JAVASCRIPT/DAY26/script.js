1bdb44cc26a354dfb48543bd355993b73e444130
ashish017-17 <ashish03724@gmail.com>
2026-02-23 23:00:06 +0530

    day27

---
+++ JAVASCRIPT/DAY26/script.js
@@ -0,0 +1,23 @@
+// alert();
+
+// var a = 12;
+// let b = 12;
+// const c = 12;
+
+// a = 32;
+// b = 64;
+// c = 12;
+
+// console.log("hey");
+// console.error("hello");
+// console.warn("heyyyy");
+
+// prompt("name ?");
+
+// alert("hello");
+
+
+//what is  strings?
+//anything which is written in "hello" is called string.
+
+//slice template-strings (template literal ``) split replace includes
