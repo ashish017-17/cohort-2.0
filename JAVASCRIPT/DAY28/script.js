28da4e728e46b20a59698a2d1f8ac868a7814457
ashish017-17 <ashish03724@gmail.com>
2026-03-21 12:35:20 +0530

    DAY29

--- JAVASCRIPT/DAY28/script.js
+++ JAVASCRIPT/DAY28/script.js
@@ -1,8 +1,16 @@
 // add comments
-// harsh
 
+// console.log("hello world");
 
-//
+//datatypes - data ka type hi hai datatype
+
+//harsh - string
+// 123 - number
+// true, false - boolean
+//[1,2,3] - array
+// {name: "harsh", age: 23} - object
+//12.34 - float
+
 // float
 // number
 // string
@@ -15,3 +23,10 @@
 // NaN
 // Infinity
 // relative and primitive datatype in js
+
+var a = [1, 2, 3, 4, 5];
+var b = a; // reference copy
+b.pop();
+
+//primitives - jinki value direct copy kar sakte hai
+// reference - jinki value copy nahi kar sakte hai, unka reference copy karte hai
