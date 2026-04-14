9363025c67d9555f09b7c82063dc60530ee9e71c
ashish017-17 <ashish03724@gmail.com>
2026-04-14 22:50:17 +0530

    DAY31

--- JAVASCRIPT/DAY30/script.js
+++ JAVASCRIPT/DAY30/script.js
@@ -1,16 +1,89 @@
-//Assignment operators
+// Assignment operators
 // = += -= *= /= %=
 
-let a = 12;
+// let a = 12;
 
 // += value ko particular se badhana
 
-a += 13;
+// a += 13;
 
-a -= 13;
+// a -= 13;
 
-a *= 13;
+// a *= 13;
 
-a /= 13;
+// a /= 13;
 
-a %= 3;
+// a %= 3;
+
+// comparison operator
+// == === != !=== > < >= <=
+
+// == not strict comapre  NEVER USE THIS
+// === strict comapre ALWAYS USE THIS 
+// != NEVER USE THIS 
+// !== ALWAYS USE THIS
+// > GRREATER
+// < SMALLER
+// >= 
+// <=
+
+// Logical Operator 
+// && || !
+
+// true  && true = true
+// false && true = false
+// true  && false = false
+// false && false = false
+// username === exist && password === userpassword
+
+// true  || true = true
+// false ||   true = true
+// true  ||   false = true
+// false ||   false = false  
+
+// ! not
+// ulta krta hai true ko false and false ko true
+
+// Ternary Operator
+// condition ? doThis : doThat
+// 12 > 13 ? console.log("hey") : console.log("hello");
+
+// Type checking operator
+// type of
+// ~~instanceof
+// kuch bhi banaaoge wo top pe likhi gayi cheejo se aata hai
+// let arr =[];
+
+// string Operator
+// +(for concatenation)
+// "harsh"+"sharma"
+// 'harshsharma'
+
+// Nullish coalescing Operator
+// ?? (fallback only When null / undefine)
+
+// opational chaining
+// ?. (safe access to nested values)
+
+// let obj = {
+//     name: {
+//         first: "harsh",
+//         second: {
+//             sf: "singh",
+//             sl: "something",
+//         },
+//     },
+// };
+
+// obj?.name?.first;
+
+//variable hoisting in js
+//hoisting ka matlab hai ki variable ko banane se pahle bhi use kiya jaa sakta hai
+//hoisting ma aapka varible toot jaata hai do hisso mein 
+//declaration top of the file jaata hai
+//initialization wahi rehta hai jaha variable tha
+// ab aapka variable error nahi deta kyuki wo humesha use hone se pahle exist kr rha tha
+
+// console.log(b);
+// var b; 
+// b = 12;
