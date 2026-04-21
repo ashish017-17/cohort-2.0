0eaec96649e7bc241abd6b7450e96c813c87c56b
ashish017-17 <ashish03724@gmail.com>
2026-04-21 22:26:58 +0530

    DAY32

---
+++ JAVASCRIPT/DAY32/script.js
@@ -0,0 +1,82 @@
+// ### Loops & Conditionals in Javascript
+
+// conditionals
+// if else
+// truthy and falsy
+// 0 "" false NaN null undefined document.all
+// kuchh bhi dikhe -> true
+
+// if(12) {
+//     console.log("hey") ;
+// } else {
+//     console.log("hello");
+// }
+
+// let a = 9;
+// if(a>10){
+//     console.log("hello");
+// }
+// else{
+//     console.log("byebye");
+// }
+
+
+// else-if
+// if (12>13) {
+//     console.log("if chala");
+// } else if (13 > 14) {
+//     console.log("else if chala");
+// } else if (14 < 15) {
+//     console.log("else if 2 chala");
+// } else {
+//     console.log("else chala");
+// }
+
+//Ternary Operator 
+//condition ? true : false
+
+//12 < 13 ? console.log("hey") : console.log("hello");
+
+//switch-case
+// switch(2){
+//     case 1:
+//         console.log("This is case 1");
+//         break;
+//     case 2:
+//         console.log("This is case 2");
+//         break;
+//     case 3:
+//         console.log("This is case 3");
+//         break; 
+//     case 4:
+//         console.log("This is case 4");
+//         break;
+//     default:
+//         console.log("This is default");
+// }
+
+
+// programming mein kai baar aisa phase aata hai jaha par aapko decide karna padta hai ki kya karein, option A choose kare ya option B choose karein
+// agar baarish ho rahi hai to print karo hello nahito print karo bye-bye
+// if else else-if ternary switch-case
+
+// user password do -> password agar sahi hai to accepted nahito error
+
+// - Understanding Condition Operator in Javascript - [`if` , `else` , `if-else` , `else-if` , `Ternary Operator` , `switch` ]
+
+
+// loops = repeat karna
+// loop ke use cases mein do prakaar
+// 1 3 5 7 9 11 13 15 17 19
+// 0 1 1 2 3 5 8 13 21 34...
+
+
+
+// - `for` Loop in JavaScript
+// - `while` Loop in JavaScript
+// - `do...while` in JavaScript
+// - `forEach` in JavaScript
+// - `for in` Loop in JavaScript
+// - `for of` Loop in JavaScript
+// - Recursion in JavaScript
+// - Loop control Statements - [`break` , `continue`]
