6ad3820a2603260c591f4d30f48f4f376a2eec14
ashish017-17 <ashish03724@gmail.com>
2026-04-25 17:27:51 +0530

    DAY33

--- JAVASCRIPT/DAY33/script.js
+++ JAVASCRIPT/DAY33/script.js
@@ -1,7 +1,66 @@
 //Level 1 - Pure Beginner Practice
-//1.  Print numbers from 1 to 10
-//Loop from 1 to 10 and Print each numbers
+// 1.  Print numbers from 1 to 10
+//    Loop from 1 to 10 and Print each numbers
 
-for(let i = 1; i <=10; i++ ){
-    console.log(i);
-}
+// for(let i = 1; i <=10; i++ ){
+//     console.log(i);
+// }
+
+
+// 2.  Print only even numbers from 1 to 20
+//     use a loop and conditions to print only even ones
+
+// for(let i =1; i<=20; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+// 3.  Print number from 10 to 1
+//     Reverse loop with a decrement.{
+
+// for (let i = 10; i>=1; i--){
+//     console.log(i);
+// }
+
+
+// 4.  Print the word "yes" 5 times
+//     Repeat using a loop.
+
+// for (let i = 1; i <=5; i++){
+//     console.log("yes");
+// }
+
+// 5.  Print Whether number from 1 to 10 are even or odd
+//     For each number, check -> "Even", else -> "odd"
+
+// for (let i = 1; i<=10; i++){
+//     if(i % 2 === 0){
+//         console.log(i,"- Even");
+//     }
+//     else{
+//         console.log(i,"- Odd");
+//     }
+// }
+
+// for (let i = 1; i<=10; i++){
+//     if(i % 2 === 0){
+//         console.log(`${i} - Even`);
+//     }
+//     else{
+//         console.log(`${i} - Odd`);
+//     }
+// }
+
+
+// 6.  Ask user for a number and say if it's positive or negative
+//     Use prompt() and a conditional.
+// let num = prompt("Enter the number");
+// num = parseInt(num);
+// if (num >= 0) {
+//     console.log("positive");
+// }
+// else{
+//     console.log("negative")
+// }
